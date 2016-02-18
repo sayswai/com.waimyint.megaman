e21f442ba45f29b0165258cc849885740a6b1391
Wai Myint <sayswai@gmail.com>
2016-02-17 19:41:30 -0800

    v1.0 working
    -All the updates from before +
    -arrow key is now bounded so the sprite won't run off the screen

--- src/main.java
+++ src/main.java
@@ -113,21 +113,21 @@ public class main {
             }
             int speed = 5;
             
-	            if (kbState[KeyEvent.VK_LEFT] || kbState[KeyEvent.VK_A] && inBounds(spritePos[0]-speed, spritePos[1], window.getWidth(), window.getHeight())) {//left
+	            if ((kbState[KeyEvent.VK_LEFT] || kbState[KeyEvent.VK_A]) && inBounds(spritePos[0]-speed, spritePos[1], window.getWidth(), window.getHeight())) {//left
 	                 spritePos[0] -= speed;
 	                 spriteTex = 2;
 	            }
 	
-	            if (kbState[KeyEvent.VK_RIGHT]|| kbState[KeyEvent.VK_D] && inBounds(spritePos[0]+speed, spritePos[1], window.getWidth(), window.getHeight())) {//right
+	            if ((kbState[KeyEvent.VK_RIGHT]|| kbState[KeyEvent.VK_D]) && inBounds(spritePos[0]+speed, spritePos[1], window.getWidth(), window.getHeight())) {//right
 	            	  spritePos[0] += speed;
 	            	  spriteTex = 1;
 	            }
 	
-	            if (kbState[KeyEvent.VK_UP]|| kbState[KeyEvent.VK_W] && inBounds(spritePos[0], spritePos[1]-speed, window.getWidth(), window.getHeight())) {//up
+	            if ((kbState[KeyEvent.VK_UP]|| kbState[KeyEvent.VK_W]) && inBounds(spritePos[0], spritePos[1]-speed, window.getWidth(), window.getHeight())) {//up
 	                spritePos[1] -= speed;
 	            }
 	
-	            if (kbState[KeyEvent.VK_DOWN]|| kbState[KeyEvent.VK_S] && inBounds(spritePos[0], spritePos[1]+speed, window.getWidth(), window.getHeight())) {//down
+	            if ((kbState[KeyEvent.VK_DOWN]|| kbState[KeyEvent.VK_S]) && inBounds(spritePos[0], spritePos[1]+speed, window.getWidth(), window.getHeight())) {//down
 	                spritePos[1] += speed;
 	            }
            
